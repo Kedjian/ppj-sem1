67ca1a56485356678c57b8283d3caab915141273
Kedjian <o.mat.makowski@gmail.com>
2025-03-24 15:13:48 +0100

    a small fix to ex3

--- src/exercises/ex3.java
+++ src/exercises/ex3.java
@@ -22,22 +22,13 @@ public class ex3 {
 
         char aChar = 'a';
 
-        // binary min and max values for int
         int valBinaryMax = 0b1111111111111111111111111111111;
-        System.out.println(valBinaryMax);
         int valBinaryMin = -0b10000000000000000000000000000000;
-        System.out.println(valBinaryMin);
 
-        // hexadecimal min and max values for int
         int valHexMax = 0x7FFFFFFF;
-        System.out.println(valHexMax);
         int valHexMin = -0x80000000;
-        System.out.println(valHexMin);
 
-        // octal min and max values for int
         int valOctalMax = 017777777777;
-        System.out.println(valOctalMax);
         int valOctalMin = -020000000000;
-        System.out.println(valOctalMin);
     }
 }
