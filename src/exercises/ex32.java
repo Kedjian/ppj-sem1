ea0146673d40a2e8d6d1a490c5a216101011bdfc
Kedjian <o.mat.makowski@gmail.com>
2025-03-27 22:58:25 +0100

    adding ex32

---
+++ src/exercises/ex32.java
@@ -0,0 +1,15 @@
+package exercises;
+
+public class ex32 {
+    public static void main(String[] args) {
+        int t2137 = 2;
+        // int 2137t = 2;
+        /// you cannot name a variable starting with numbers
+        // int @AzBzCz = 2;
+        /// you cannot name a variable starting with special letters
+        // int static = 2;
+        /// you cannot name a variable using a keyword
+        // int null = 2;
+        /// you cannot name a variable using a keyword
+    }
+}
