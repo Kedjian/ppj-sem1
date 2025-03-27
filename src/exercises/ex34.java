98a1de2095413e7dc7f30bf7ec9cf913bf16adba
Kedjian <o.mat.makowski@gmail.com>
2025-03-27 23:04:51 +0100

    adding ex34

---
+++ src/exercises/ex34.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class ex34 {
+    public static void main(String[] args) {
+        char charValue = 'a';
+        char charvalue = 'b';
+
+        /// variable names are different
+        /// lower and upper case matter
+        System.out.println(charValue);
+        System.out.println(charvalue);
+    }
+}
