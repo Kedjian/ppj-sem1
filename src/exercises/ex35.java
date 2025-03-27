fd16364940e09e1a1992d69cc6f286ac52cdd3d6
Kedjian <o.mat.makowski@gmail.com>
2025-03-27 23:29:49 +0100

    adding ex35

---
+++ src/exercises/ex35.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class ex35 {
+    public static void main(String[] args) {
+        int a = 2;
+        double b = 3.14;
+        /// a = b;
+        b = a;
+
+        // it's impossible to turn a double variable into an integer variable
+        // without using casting, because they store fundamentally different
+        // parameters.
+    }
+}
