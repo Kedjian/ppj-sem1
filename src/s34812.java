f105b5ebb883d8958407f20bb45894c957cc2b06
Kedjian <o.mat.makowski@gmail.com>
2025-04-01 14:50:24 +0200

    clearing the main path

--- src/s34812.java
+++ src/s34812.java
@@ -1,24 +1,5 @@
 public class s34812 {
     public static void main(String[] args) {
-        // simplify (!(a < b) && !(a > b))
 
-        int a = 1;
-        int b = 2;
-
-        if (a == b) {
-            System.out.println("See? That's easier!");
-        }
-
-        // !(x < y) translates to: IT IS NOT true that x is less than y
-        // mathematically speaking, that's like saying x >= y
-
-        // !(x > y) translates to: IT IS NOT true that x is greater than y
-        // mathematically speaking, that's like saying x <= y
-
-        // if both conditions need to be true then
-        ///     x cannot be less than y
-        // while at the same time
-        ///     x cannot be greater than y
-        // which translates to their equality
     }
 }
