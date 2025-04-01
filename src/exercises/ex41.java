36bea3bb3861ff99e90b10cdc5541a13d8577c05
Kedjian <o.mat.makowski@gmail.com>
2025-04-01 14:30:36 +0200

    adding ex41

---
+++ src/exercises/ex41.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class ex41 {
+    public static void main(String[] args) {
+        double a = 0;
+        double x;
+        if (a >= 0) x = 1.701;
+        if (a < 0) x = 2D*3.14f;
+        // System.out.println(x);
+
+        // the double x variable was never initialized,
+        // so it cannot be changed.
+    }
+}
