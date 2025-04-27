f53e8b48492b58b93e6a8b9817f78586c8f5eec2
Kedjian <o.mat.makowski@gmail.com>
2025-04-27 23:51:38 +0200

    added ex63

---
+++ src/exercises/ex63.java
@@ -0,0 +1,47 @@
+package exercises;
+
+public class ex63 {
+    public static void main(String[] args) {
+        double[] varArray = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        int max = 10;
+        int min = 1;
+        int range = max - min + 1;
+
+        for (int i = 0; i < varArray.length; i++) {
+            double rand = (Math.random() * range) + min;
+            varArray[i] = rand;
+        }
+
+        System.out.println(" ");
+
+        for (double v : varArray) {
+            System.out.print(v + " ");
+        }
+
+        System.out.println(" ");
+
+        for (int i = 0; i < varArray.length; i++) {
+            if (checkIfEven(i)) {
+                System.out.print(varArray[i] + " ");
+            } else {
+                System.out.print("");
+            }
+        }
+
+        System.out.println(" ");
+
+        for (double v : varArray) {
+            int iteratedNum = (int) v;
+            if (!checkIfEven(iteratedNum)) {
+                System.out.print(iteratedNum + " ");
+            } else {
+                System.out.print("");
+            }
+        }
+    }
+
+    public static boolean checkIfEven(double number) {
+        return number % 2 == 0;
+    }
+}
