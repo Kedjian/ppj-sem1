f26698a2baf1e548eae8c6d6e4d6085fe763dbfd
Kedjian <o.mat.makowski@gmail.com>
2025-04-27 23:13:40 +0200

    added ex6

--- src/exercises/ex6.java
+++ src/exercises/ex6.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+package exercises;
 
-public class s34812 {
+public class ex6 {
     public static void main(String[] args) {
         int[] variables = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     }
